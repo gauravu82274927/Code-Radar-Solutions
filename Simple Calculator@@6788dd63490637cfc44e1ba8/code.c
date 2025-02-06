9826f182e4be938bd3a0db69904cc28b597fc907
Gaurav <gauravkhatri492006@gmail.com>
2025-02-06 10:43:46 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -3,10 +3,10 @@ int main(){
     int a,b,d,e,f,g;
     char c;
     scanf("%d %d %c",&a,&b,&c);
-    d=a+b;
-    e=a-b;
-    f=a*b;
-    g=a/b;
+    d=(a+b);
+    e=(a-b);
+    f=(a*b);
+    g=(a/b);
 
     if(c=="+"){
         printf("%d",d);
